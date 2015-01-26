b7650aa98952b621bac7a4789f3396ac8e3be812
wangyu <wangyu@itianpin.com>
2015-01-27 01:00:19 +0800

    fixbug

---
+++ config-sample.py
@@ -0,0 +1,7 @@
+#-*-coding:utf-8-*-
+__author__ = 'wangyu'
+
+class Config(object):
+    db = "mysql://root:1234@localhost/ufind?charset=utf8"
+    SUCCESS_KEY = "@åßßßßßå∂åß∂œ∑œ∑œ∑œ∑œœ∑åßåß∂åß∂ΩΩΩ≈çΩ≈ç"
+    REDIS_URL = "redis:://:"
