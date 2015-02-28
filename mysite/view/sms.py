797cc134b2722fd2c72a91085890798eb4003eb1
wangyu <wangyu@itianpin.com>
2015-02-28 15:47:58 +0800

    json

--- mysite/view/sms.py
+++ mysite/view/sms.py
@@ -53,6 +53,7 @@ def send_sms():
         print data.get("phonenum")
         print data.getlist
         print data.to_dict()
+        print data.fromkeys
         print data.itervalues()
         print dir(request)
         phone = request.form[0]["phonenum"]
