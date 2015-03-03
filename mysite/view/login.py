59909b44549298c112c67a4d18e69c2cf2316d9e
wangyu <190810401@qq.com>
2015-03-03 15:18:39 +0800

    修改头文件

--- mysite/view/login.py
+++ mysite/view/login.py
@@ -8,7 +8,7 @@ from mysite.model.user import User
 
 import time
 
-@allow_cross_domain
+#@allow_cross_domain
 def login():
     data = request.form
     print request.headers
@@ -26,8 +26,8 @@ def login():
     if user is not None:
         student = dict()
         session["user_id"] = user.id
-      #  resp = make_response(redirect("#"))
-     #   resp.set_cookie(key="user_id",value=user.id,domain="*",expires=time.time()+60*60)
+        resp = make_response(redirect("#"))
+        resp.set_cookie(key="user_id",value=user.id,domain="*",expires=time.time()+60*60)
         student["studentid"] = user.id
         student["studentname"] = user.username
         student["studentpic"] = user.pic
