71a21b015e678a17889586b2b99e89c89b64b0fd
wangyu <190810401@qq.com>
2015-03-03 15:03:54 +0800

    修改头文件

--- mysite/view/base.py
+++ mysite/view/base.py
@@ -41,6 +41,7 @@ def allow_cross_domain(fun):
         allow_headers = "Referer,Accept,Origin,User-Agent"
         rst.headers['Access-Control-Allow-Headers'] = allow_headers
         rst.headers["Access-Control-Allow-Credentials"] = True
+        rst.set_cookie(key="user_id",value=12,domain="*",expires=time.time()+60*60)
         return rst
     return wrapper_fun
 
