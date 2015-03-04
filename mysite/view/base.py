c368e68cdec828209d94db638cbbab935afd3ed3
wangyu <190810401@qq.com>
2015-03-04 13:46:41 +0800

    短信接口加上字段

--- mysite/view/base.py
+++ mysite/view/base.py
@@ -1,7 +1,7 @@
 __author__ = 'wangyu'
-from flask import session,wrappers,redirect
+from flask import session, redirect
 from functools import wraps
-from flask import make_response,jsonify
+from flask import make_response, jsonify, g
 
 import time
 from itsdangerous import Signer
@@ -9,22 +9,23 @@ from itsdangerous import Signer
 from mysite.model.user import User
 from config import Config
 
+
 def validate_user_login(func):
     @wraps(func)
-    def _validate_user_login(*args,**kwargs):
+    def _validate_user_login(*args, **kwargs):
         if "username" in session:
-            return func(*args,**kwargs)
+            return func(*args, **kwargs)
         return redirect("/login")
     return _validate_user_login
 
 
 def login_user_info(func):
     @wraps(func)
-    def _login_user_info(*args,**kwargs):
+    def _login_user_info(*args, **kwargs):
         if "user_id" in session:
-            user = User.get_user_info(session["user_id"])
+            user = User.get_user_info(g.db,session["user_id"])
             student = dict()
-            student["studentid"] =user.id
+            student["studentid"] = user.id
             student["studentname"] = user.username
             student["studentpic"] = user.pic
             return jsonify(status="success",
@@ -42,14 +43,11 @@ def allow_cross_domain(fun):
         allow_headers = "Referer,Accept,Origin,User-Agent"
         rst.headers['Access-Control-Allow-Headers'] = allow_headers
         rst.headers["Access-Control-Allow-Credentials"] = True
- #      cookie = {"user_id":"123"}
- #       rst.headers["Set-Cookie"] = cookie
-        #rst.set_cookie(key="user_id",value="12",domain="*",expires=time.time()+60*60)
 
         return rst
     return wrapper_fun
 
-def get_university_img(universityname,num):
+def get_university_img(universityname, num):
     return "http://www.22too.com/US(150115)/intimer/"+str(universityname)+"/"+str(num)+".jpg"
 
 
@@ -61,9 +59,11 @@ def get_timestamp(create_time):
     timeArray = time.localtime(create_time)
     return time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
 
+
 def set_sign_safe(sign_file):
     s = Signer(Config.login_sign)
     return s.sign(sign_file)
+
 
 def get_sign_safe(true_file):
     s= Signer(Config.login_sign)
