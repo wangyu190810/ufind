897bd37e3c09b0e9d8b1c144d50d3d7761a78f72
wangyu <190810401@qq.com>
2015-03-06 18:13:30 +0800

    用户详细信息bug 修复

--- mysite/view/user.py
+++ mysite/view/user.py
@@ -18,16 +18,16 @@ def get_user_info():
         print request.args
         student_id = request.args.get("studentid")
         student_info = {}
-        for row in User.get_user_info(g.db, student_id):
-            student_info["prevuniversity"] = row.prevuniversity
-            student_info["name"] = row.username
-            student_info["prevmajor"] = row.prevmajor
-            student_info["GPA"] = row.GPA
-            student_info["TOEFL"] = row.TOEFL
-            student_info["IELTS"] = row.IELTS
-            student_info["GMAT"] = row.GMAT
-            student_info["SAT"] = row.SAT
-            student_info["pic"] = row.pic
+        row = User.get_user_info(g.db, student_id)
+        student_info["prevuniversity"] = row.prevuniversity
+        student_info["name"] = row.username
+        student_info["prevmajor"] = row.prevmajor
+        student_info["GPA"] = row.GPA
+        student_info["TOEFL"] = row.TOEFL
+        student_info["IELTS"] = row.IELTS
+        student_info["GMAT"] = row.GMAT
+        student_info["SAT"] = row.SAT
+        student_info["pic"] = row.pic
         universityname = []
         student_info["universityname"] = universityname
         return json.dumps(student_info)
