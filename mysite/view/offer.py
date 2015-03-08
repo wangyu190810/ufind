fc79a470715fad9a19b68993b5f2530e40862050
wangyu <190810401@qq.com>
2015-03-09 02:15:54 +0800

    找到分数插入不正确的原因，然后修正

--- mysite/view/offer.py
+++ mysite/view/offer.py
@@ -25,6 +25,7 @@ def set_offer():
             offer_grade = data.get("offers["+str(num)+"][grade]")
             offer_university_id = data.get("offers["+str(num)+"][universityid]")
             offer_type = data.get("'offers["+str(num)+"][offertype]")
+            print data.get("offers[0][offertype]")
             scholarship_type = data.get("offers["+str(num)+"][scholarship][type]")
             scholarship_money = data.get("offers["+str(num)+"][scholarship][money]")
             if offer_major_id is None:
