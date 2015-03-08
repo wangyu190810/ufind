572e488520a4786404ddcf0e581e0277e62d5d53
wangyu <190810401@qq.com>
2015-03-08 21:01:18 +0800

    修复bug

--- mysite/view/score.py
+++ mysite/view/score.py
@@ -97,80 +97,80 @@ def set_user_score():
                                     GMAT_ir=GMATmoreIR
                                     )
 
-
-        #else:TOEFLmore
-
-        #data = json.loads(data)
-        #print data
-
-        if data["type"] == "1":
-            prevuniversity = data[u"prevuniversity"]
-            prevmajor = data["prevmajor"]
-            GPA = data["GPA"]
-            rank = data["rank"]
-            TOEFL = data["TOEFL"]
-            IELTS = data["IELTS"]
-            GRE  =data["GRE"]
-            GMAT = data["GMAT"]
-            description = data["description"]
-            user_id = session["user_id"]
-            User.set_user_info_detail(g.db,
-                                      user_id=user_id,
-                                      prevuniversity=prevuniversity,
-                                      prevmajor=prevmajor,
-                                      type=int(data["type"]),
-                                      description=description)
-            Score.set_user_info(g.db,university_type=1,
-                                user_id=user_id,
-                                rank=rank,
-                                TOEFL_r=int(TOEFL["r"]),
-                                TOEFL_l=int(TOEFL["l"]),
-                                TOEFL_s=int(TOEFL["s"]),
-                                TOEFL_w=int(TOEFL["w"]),
-                                IELTS_r=int(IELTS["r"]),
-                                IELTS_l=int(IELTS["l"]),
-                                IELTS_s=int(IELTS["s"]),
-                                IELTS_w=int(IELTS["w"]),
-                                GRE_v=int(GRE["v"]),
-                                GRE_q=int(GRE["q"]),
-                                GRE_aw=float(GRE["aw"]),
-                                GMAT_v=int(GMAT["v"]),
-                                GMAT_q=int(GMAT["q"]),
-                                GMAT_aw=float(GMAT["aw"]),
-                                GMAT_ir=int(GMAT["ir"])
-                                )
-        else:
-            prevuniversity = data[u"prevuniversity"]
-            prevmajor = data["prevmajor"]
-            GPA = data["GPA"]
-            rank = data["rank"]
-            TOEFL = data["TOEFL"]
-            IELTS = data["IELTS"]
-            SAT  = data["SAT"]
-            SATSUB = data["SATSUB"]
-            user_id = session["user_id"]
-            description = data["description"]
-            User.set_user_info_detail(g.db,
-                                      user_id=user_id,
-                                      prevuniversity=prevuniversity,
-                                      prevmajor=prevmajor,
-                                      type=int(data["type"]),
-                                      description=description)
-            Score.set_user_info(g.db,
-                                university_type=0,
-                                rank=rank,
-                                user_id=user_id,
-                                TOEFL_r=int(TOEFL["r"]),
-                                TOEFL_l=int(TOEFL["l"]),
-                                TOEFL_s=int(TOEFL["s"]),
-                                TOEFL_w=int(TOEFL["w"]),
-                                IELTS_r=int(IELTS["r"]),
-                                IELTS_l=int(IELTS["l"]),
-                                IELTS_s=int(IELTS["s"]),
-                                IELTS_w=int(IELTS["w"]),
-                                SAT_cr=int(SAT["cr"]),
-                                SAT_w=int(SAT["w"]),
-                                SAT_m=int(SAT["m"])
-                                )
+#
+#        #else:TOEFLmore
+#
+#        #data = json.loads(data)
+#        #print data
+#
+#        if data["type"] == "1":
+#            prevuniversity = data[u"prevuniversity"]
+#            prevmajor = data["prevmajor"]
+#            GPA = data["GPA"]
+#            rank = data["rank"]
+#            TOEFL = data["TOEFL"]
+#            IELTS = data["IELTS"]
+#            GRE  =data["GRE"]
+#            GMAT = data["GMAT"]
+#            description = data["description"]
+#            user_id = session["user_id"]
+#            User.set_user_info_detail(g.db,
+#                                      user_id=user_id,
+#                                      prevuniversity=prevuniversity,
+#                                      prevmajor=prevmajor,
+#                                      type=int(data["type"]),
+#                                      description=description)
+#            Score.set_user_info(g.db,university_type=1,
+#                                user_id=user_id,
+#                                rank=rank,
+#                                TOEFL_r=int(TOEFL["r"]),
+#                                TOEFL_l=int(TOEFL["l"]),
+#                                TOEFL_s=int(TOEFL["s"]),
+#                                TOEFL_w=int(TOEFL["w"]),
+#                                IELTS_r=int(IELTS["r"]),
+#                                IELTS_l=int(IELTS["l"]),
+#                                IELTS_s=int(IELTS["s"]),
+#                                IELTS_w=int(IELTS["w"]),
+#                                GRE_v=int(GRE["v"]),
+#                                GRE_q=int(GRE["q"]),
+#                                GRE_aw=float(GRE["aw"]),
+#                                GMAT_v=int(GMAT["v"]),
+#                                GMAT_q=int(GMAT["q"]),
+#                                GMAT_aw=float(GMAT["aw"]),
+#                                GMAT_ir=int(GMAT["ir"])
+#                                )
+#        else:
+#            prevuniversity = data[u"prevuniversity"]
+#            prevmajor = data["prevmajor"]
+#            GPA = data["GPA"]
+#            rank = data["rank"]
+#            TOEFL = data["TOEFL"]
+#            IELTS = data["IELTS"]
+#            SAT  = data["SAT"]
+#            SATSUB = data["SATSUB"]
+#            user_id = session["user_id"]
+#            description = data["description"]
+#            User.set_user_info_detail(g.db,
+#                                      user_id=user_id,
+#                                      prevuniversity=prevuniversity,
+#                                      prevmajor=prevmajor,
+#                                      type=int(data["type"]),
+#                                      description=description)
+#            Score.set_user_info(g.db,
+#                                university_type=0,
+#                                rank=rank,
+#                                user_id=user_id,
+#                                TOEFL_r=int(TOEFL["r"]),
+#                                TOEFL_l=int(TOEFL["l"]),
+#                                TOEFL_s=int(TOEFL["s"]),
+#                                TOEFL_w=int(TOEFL["w"]),
+#                                IELTS_r=int(IELTS["r"]),
+#                                IELTS_l=int(IELTS["l"]),
+#                                IELTS_s=int(IELTS["s"]),
+#                                IELTS_w=int(IELTS["w"]),
+#                                SAT_cr=int(SAT["cr"]),
+#                                SAT_w=int(SAT["w"]),
+#                                SAT_m=int(SAT["m"])
+#                                )
         return jsonify(status="success")
 
