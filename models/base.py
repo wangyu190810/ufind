39d32391f88979ead4b5f76d213f61205ac3f6c4
Ma Ming <khahux@163.com>
2015-04-06 23:29:22 +0800

    'init'

--- models/base.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-# email: khahux@163.com
-
-from sqlalchemy.schema import MetaData
-from sqlalchemy.ext.declarative import declarative_base
-
-
-metadata = MetaData()
-Base = declarative_base()
