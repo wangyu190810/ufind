9e4381e37a10c56dacb9aa649f557ce0f12bf21b
wangyu <190810401@qq.com>
2015-04-07 20:40:51 +0800

    sql 操作

--- mysite/model/offer.py
+++ mysite/model/offer.py
@@ -109,19 +109,34 @@ class Offer(Base):
 
         sql = text("""(select count(user_id) as countmajor,major_id as id from offer
         where university_id=:university_id_1 and  school1_id=:school_id_1
-        group by major_id
-        order by countmajor desc)
-        union
+        group by major_id order by countmajor desc) union
         (select 0 as countmajor, id from major
         where university_id=:university_id_2 and  faculty_id=:school_id_2 and id not
         in (select major_id from offer where university_id=:university_id_3 and
-        school1_id=:school_id_3)
-        order by rand()
-        limit 3)""")
+        school1_id=:school_id_3) order by rand() limit 3)""")
+
+        sql = """(select count(user_id) as countmajor,major_id as id from offer
+        where university_id=%s and  school1_id=%s
+        group by major_id order by countmajor desc) union
+        (select 0 as countmajor, id from major
+        where university_id=%s and  faculty_id=%s and id not
+        in (select major_id from offer where university_id=%s and
+        school1_id=%s) order by rand() limit 3)"""
+        # return connection.execute(sql,
+        #                           university_id_1=university_id,
+        #                           school_id_1=school_id,
+        #
+        #                           university_id_2=university_id,
+        #                           school_id_2=school_id,
+        #
+        #                           university_id_3=university_id,
+        #                           school_id_3=school_id)
         return connection.execute(sql,
-                                  university_id_1=university_id,
-                                  school_id_1=school_id,
-                                  university_id_2=university_id,
-                                  school_id_2=school_id,
-                                  university_id_3=university_id,
-                                  school_id_3=school_id)
+                                  university_id,
+                                  school_id,
+
+                                  university_id,
+                                  school_id,
+
+                                  university_id,
+                                  school_id)
