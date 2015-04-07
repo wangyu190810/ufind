03c202263a9a5a536ea97cb5d0bd1bded5748ebb
wangyu <190810401@qq.com>
2015-04-07 19:03:17 +0800

    热门专业！sql处理

--- mysite/model/offer.py
+++ mysite/model/offer.py
@@ -96,9 +96,16 @@ class Offer(Base):
         connection.commit()
 
     @classmethod
-    def get_index_from_offer_num(cls, connection, university_id, faculty_id):
-        sql = text("select count(user_id) as countmajor, major_id from offer"
-                   " where university_id = :university_id and faculty_id "
-                   "= :faculty_id group by major_id order by countmajor")
-        return connection.execute(sql, university_id=university_id,
-                                  faculty_id=faculty_id)
+    def get_index_from_offer_num(cls, connection):
+        sql = text("""(select count(user_id) as countmajor,major_id as id from offer
+        where university_id=2 and  school1_id=1
+        group by major_id
+        order by countmajor desc)
+        union
+        (select 0 as countmajor, id from major
+        where university_id=2 and  faculty_id=1 and id not in (select major_id from offer
+        where university_id=2 and  school1_id=1)
+        order by rand()
+        limit 3)""")
+        return connection.execute(sql)#, university_id=university_id,
+                                  #faculty_id=faculty_id)
