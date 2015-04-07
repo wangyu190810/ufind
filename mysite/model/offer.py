4f74f0d06dac1aed1a20ae2bb38cc7a7fdd831ac
wangyu <190810401@qq.com>
2015-04-07 21:04:49 +0800

    sql 操作

--- mysite/model/offer.py
+++ mysite/model/offer.py
@@ -115,7 +115,12 @@ class Offer(Base):
         in (select major_id from offer where university_id=:university_id_3 and
         school1_id=:school_id_3) order by rand() limit 3)""")
 
-        sql = """(select count(user_id) as countmajor,major_id as id from offer where university_id=%s and  school1_id=%s group by major_id order by countmajor desc) union (select 0 as countmajor, id from major where university_id=%s and  faculty_id=%s and id not in (select major_id from offer where university_id=%s and school1_id=%s) order by rand() limit 3)"""
+        sql = """(select count(user_id) as countmajor,major_id as id
+        from offer where university_id=%s and  school1_id=%s group by
+        major_id order by countmajor desc) union (select 0 as countmajor,
+        id from major where university_id=%s and  faculty_id=%s and id not in
+        (select major_id from offer where university_id=%s and school1_id=%s)
+        order by rand() limit 3)""" %(university_id,school_id,university_id,school_id,university_id,school_id)
         # return connection.execute(sql,
         #                           university_id_1=university_id,
         #                           school_id_1=school_id,
@@ -126,12 +131,13 @@ class Offer(Base):
         #                           university_id_3=university_id,
         #                           school_id_3=school_id)
         print university_id,school_id
-        return connection.execute(sql,(
-                                  university_id,
-                                  school_id,
-
-                                  university_id,
-                                  school_id,
-
-                                  university_id,
-                                  school_id))
+        return connection.execute(sql)
+        # return connection.execute(sql,(
+        #                           university_id,
+        #                           school_id,
+        #
+        #                           university_id,
+        #                           school_id,
+        #
+        #                           university_id,
+        #                           school_id))
