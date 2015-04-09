e3cc64c3bd295523dade4e422a11c1bd9e85771f
wangyu <190810401@qq.com>
2015-04-09 11:56:46 +0800

    wechat 标准化

--- mysite/view/base.py
+++ mysite/view/base.py
@@ -71,9 +71,9 @@ def get_university_twodim(universityname):
 
 def set_university_offer_wechat(universityname,user_type,num):
     if user_type == 0:
-        wechat_type = "Bachelor"
+        wechat_type = "_Bachelor"
     else:
-        wechat_type = "Master_PHD"
+        wechat_type = "_Master_PHD"
     print universityname,user_type,num
     print type(universityname),type(user_type),type(num)
     return u"http://www.ufindoffer.com/images/unimg/twodim/"+universityname+wechat_type+str(num)+".jpg"
