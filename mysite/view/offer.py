6e15cab0180bbe5406ad06004c9423927dcb6ce7
wangyu <190810401@qq.com>
2015-04-09 20:09:15 +0800

    grade phd

--- mysite/view/offer.py
+++ mysite/view/offer.py
@@ -20,6 +20,7 @@ from mysite.model.state import State
 def set_offer():
     if request.method == "POST":
         data = request.form
+        print data
         user_id = session["user_id"]
         user = User.get_user_info(g.db,user_id)
         user_type = None
@@ -29,7 +30,7 @@ def set_offer():
         num = 0
         while True:
             offer_major_id = data.get("offers["+str(num)+"][majorid]")
-            offer_grade = data.get("offers["+str(num)+"][grade]","Bachelor")
+            offer_grade = data.get("offers["+str(num)+"][grade]","PhD")
             offer_university_id = data.get("offers["+str(num)+"][universityid]")
             offer_type = data.get("offers["+str(num)+"][offertype]")
             scholarship_type = data.get("offers["+str(num)+"][scholarship][type]")
