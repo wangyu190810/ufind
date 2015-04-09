55a481953c5d3c164ce5fdbb8f43b355cd9e4512
wangyu <190810401@qq.com>
2015-04-09 14:47:44 +0800

    平均GPA,保留两位小数

--- mysite/view/offer.py
+++ mysite/view/offer.py
@@ -123,26 +123,30 @@ def set_offer():
                 GPA_1_num == 1
             print offer_num,GPA_0_num,GPA_1_num
             try:
-                GPA = float("%2f" %offer_GPA/float(offer_num))
+                GPA = offer_GPA/float(offer_num)
             except ZeroDivisionError:
                 GPA = 0
             try:
-                GPA_0 = float("%2f" %offer_GPA_0/float(GPA_0_num))
+                GPA_0 = offer_GPA_0/float(GPA_0_num)
             except ZeroDivisionError:
                 GPA_0 = 0
             try:
-                GPA_1 = float("%2f" %offer_GPA_1/float(GPA_1_num))
+                GPA_1 = offer_GPA_1/float(GPA_1_num)
             except ZeroDivisionError:
                 GPA_1 = 0
             try:
-                TOEFL = float("%2f" %offer_TOEFL/float(offer_num))
+                TOEFL = offer_TOEFL/float(offer_num)
             except ZeroDivisionError:
                 TOEFL = 0
             try:
-                IELTS = float("%2f" %offer_IELTS/float(offer_num))
+                IELTS = offer_IELTS/float(offer_num)
             except ZeroDivisionError:
                 IELTS = 0
-            print offer_university_id,GPA,GPA_0,GPA_1,TOEFL,IELTS
+            GPA = float("%2f" %GPA)
+            GPA_0= float("%2f" %GPA_0)
+            GPA_1 = float("%2f" %GPA_1)
+            TOEFL == float("%2f" %TOEFL)
+            IELTS = float("%2f" %IELTS)
             University.set_GPA_TOEFL_IELTS(g.db,offer_university_id,GPA,GPA_0,GPA_1,TOEFL,IELTS)
         offer_list = list()
         for row_user in Offer.get_offer_student_info(g.db,user_id):
