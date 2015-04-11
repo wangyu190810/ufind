9e28600d6d85decf92247b8692536d548db0ebf3
wangyu <190810401@qq.com>
2015-04-11 12:43:43 +0800

    地图学院没有登录报错

--- mysite/view/major.py
+++ mysite/view/major.py
@@ -115,12 +115,16 @@ def get_major_from_university_faculty():
                     for row_major in Offer.get_user_id_from_major(g.db,row.id,user_type):
                         student_info = dict()
                         user = User.get_user_info(g.db,row_major.user_id)
-                        student_info["studentid"] = user.id
-                        student_info["name"] = user.username
-                        student_info["studentimg"] = user.pic
-                        student_info["GPA"] = user.GPA
-                        student_info["prevuniversity"] = user.prevuniversity
-                        students.append(student_info)
+                        if user is None:
+                            students.append(student_info)
+                        else:
+                            students.append(student_info)
+                            student_info["studentid"] = user.id
+                            student_info["name"] = user.username
+                            student_info["studentimg"] = user.pic
+                            student_info["GPA"] = user.GPA
+                            student_info["prevuniversity"] = user.prevuniversity
+                            students.append(student_info)
                         major_info["students"] = students
                     if major_info.get("students") is not None:
                         major_list.append(major_info)
