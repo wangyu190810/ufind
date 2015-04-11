ea85c686ac1359c35b0cc2dc33b16b00767529bc
wangyu <190810401@qq.com>
2015-04-11 14:05:16 +0800

    热门专业搜索区分大学和高中

--- mysite/model/offer.py
+++ mysite/model/offer.py
@@ -137,7 +137,7 @@ class Offer(Base):
         connection.commit()
 
     @classmethod
-    def get_index_from_offer_num(cls, connection,university_id,school_id):
+    def get_index_from_offer_num(cls, connection,university_id,school_id,user_type):
 
         sql = text("""(select count(user_id) as countmajor,major_id as id from offer
         where university_id=:university_id_1 and  school1_id=:school_id_1
@@ -148,11 +148,14 @@ class Offer(Base):
         school1_id=:school_id_3) order by rand() limit 3)""")
 
         sql = """(select count(user_id) as countmajor,major_id as id
-        from offer where university_id=%s and  school1_id=%s group by
+        from offer where university_id=%s and  school1_id=%s and user_type =%s group by
         major_id order by countmajor desc) union (select 0 as countmajor,
-        id from major where university_id=%s and  faculty_id=%s and id not in
-        (select major_id from offer where university_id=%s and school1_id=%s)
-        order by rand() limit 8)""" %(university_id,school_id,university_id,school_id,university_id,school_id)
+        id from major where university_id=%s and  faculty_id=%s and major_type = %s
+        and id not in (select major_id from offer where university_id=%s and
+        school1_id=%s and user_type=%s)
+        order by rand() limit 8)""" %(university_id,school_id,user_type,
+                                      university_id,school_id,user_type,
+                                      university_id,school_id,user_type)
 
 
         return connection.execute(sql)
