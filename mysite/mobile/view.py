89d4ca923a2a80c3e236a9a563a717c89e6fdc34
wangyu <190810401@qq.com>
2015-04-21 03:27:05 +0800

    抽奖部分的api完成

--- mysite/mobile/view.py
+++ mysite/mobile/view.py
@@ -117,13 +117,19 @@ def get_user_prize():
         if user:
             if user.coupon is not None:
                 return jsonify(status="user_have_coupon")
-        prize = Prize.get_random_prize(g.db)
-        print prize
+        for row in  Prize.get_random_prize(g.db):
+            prize = row
+            break
         print type(prize)
+        if prize is None:
+            return jsonify(status="success",
+                       acount=None
+                       )
         Prize.set_prize_user(g.db,prize.id,User.id)
         User.set_user_account(g.db,phone,prize.coupon,prize.account)
+
         return jsonify(status="success",
-                       count=prize.account
+                       acount=prize.account
                        )
     return jsonify(status="false")
 
