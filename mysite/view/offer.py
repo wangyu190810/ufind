691bcf1567c53568df7dc0f183a6b9aa679e0530
wangyu <190810401@qq.com>
2015-04-23 21:31:14 +0800

    add major

--- mysite/view/offer.py
+++ mysite/view/offer.py
@@ -61,7 +61,6 @@ def set_offer():
                                         faculty_id=faculty_id,
                                 )
             print id_major
-            print type(id_major),dir(id_major),id_major.id
             User.update_user_grade(g.db,user_id=user_id,grade=offer_grade)
             Offer.del_same_offer(g.db,university_id=offer_university_id,
                                 major_id =offer_major_id,user_id=user_id)
