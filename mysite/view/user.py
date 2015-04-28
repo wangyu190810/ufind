49ec54e45067ddb5338c3146bdff1f0e6ac75845
wangyu <190810401@qq.com>
2015-04-28 16:13:08 +0800

    将零和null处理

--- mysite/view/user.py
+++ mysite/view/user.py
@@ -170,11 +170,11 @@ def get_user_detail_info():
         coupons = dict()
 
         print user_info.account,user.active,
-        if user_info.active == 1 and user_info.account != 0:
+        if user_info.active == 1 and (user_info.account != 0 or user_info.active is not None):
             coupons["code"] = user_info.coupon
             coupons["account"] = user_info.account
             student_info["coupons"] = coupons
-        elif user_info.active == 2 and user_info.account != 0:
+        elif user_info.active == 2 and (user_info.account != 0 or user_info.active is not None):
             coupons["code"] = None
             coupons["account"] = user_info.account
             student_info["coupons"] = coupons
